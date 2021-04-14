f15b22ee07da9412542af46630b0d5b7f98d0913
wnsals411 <wnsals411@naver.com>
2021-04-15 00:50:31 +0900

    css 수정 및 main페이지 인기게시물로 변경

--- trendy/main/views.py
+++ trendy/main/views.py
@@ -4,13 +4,20 @@ from notice.models import Notice, Qna
 from main.models import search_word
 from django.views.generic import ListView
 from django.db.models import Q
+from django.utils import timezone
+import datetime
 
 
 def main(request):
-    Main_Notice = Notice.objects.order_by('-id')[0:4] # 메인페이지 게시물 4개 최신순
+    # Main_Notice = Notice.objects.order_by('-id')[0:4] # 메인페이지 게시물 4개 최신순
+    Hot_QNA = Qna.objects.filter(create_at__gte=timezone.now()-datetime.timedelta(days=7)).order_by('-hits')[0:4] # QNA 게시물 조회수 순 4개 (현재날짜부터 7일까지 데이터중)
     Rank = search_word.objects.order_by('-hits')[0:10] # 랭킹 10개 검색횟수 많은순
 
-    return render(request, 'main/main.html', {'MainNotice':Main_Notice, 'Rank':Rank})
+    return render(request, 'main/main.html', {
+        # 'MainNotice':Main_Notice, 
+        'Rank':Rank,
+        'HotQNA':Hot_QNA,
+        })
 
 class SearchView(ListView): # 게시글
     model = search_word
