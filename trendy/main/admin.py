4e3cc6b552de355c067b9bffe4fc3e94250d39ed
wnsals411 <wnsals411@naver.com>
2021-05-08 11:47:17 +0900

    3차 수정

--- trendy/main/admin.py
+++ trendy/main/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 from main.models import search_word
 # Register your models here.
 
-admin.site.register(search_word)
+
+class search_wordAdmin(admin.ModelAdmin):
+    fields = ['keyword', 'hits']
+    list_display = ('keyword', 'hits')
+
+
+admin.site.register(search_word, search_wordAdmin)
+
