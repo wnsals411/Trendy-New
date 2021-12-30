8eb31cac1cb66782cd40506d6e3c0bbfda7f9db3
Jun Min <wnsals411@naver.com>
2021-12-30 17:23:40 +0900

    2021-12-30

--- trendy/main/views.py
+++ trendy/main/views.py
@@ -10,14 +10,12 @@ import datetime
 
 def main(request):
     # Main_Notice = Notice.objects.order_by('-id')[0:4] # 메인페이지 게시물 4개 최신순
-    #Hot_QNA = Qna.objects.filter(create_at__gte=timezone.now()-datetime.timedelta(days=7)).order_by('-hits')[0:4]
-    Hot_QNA = Qna.objects.filter(create_at__gte=timezone.now()-datetime.timedelta(days=29)).order_by('-hits')[0:4]
+    Hot_QNA = Qna.objects.filter(create_at__gte=timezone.now()-datetime.timedelta(days=7)).order_by('-hits')[0:4]
     # QNA 게시물 조회수 순 4개 (현재날짜부터 7일까지 데이터중)
     Rank = search_word.objects.order_by('-hits')[0:10] # 랭킹 10개 검색횟수 많은순
-    #time1 = timezone.now()
-    time1 = timezone.now()-datetime.timedelta(days=22)
-    #time7 = timezone.now()-datetime.timedelta(days=7)
-    time7 = timezone.now()-datetime.timedelta(days=29)
+    time1 = timezone.now()
+    time7 = timezone.now()-datetime.timedelta(days=7)
+    
 
 
     return render(request, 'main/main.html', {
